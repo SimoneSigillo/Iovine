c87f3806b0a7c5e6e09eed91ea23125745037cf9
ettoreiovine <75394838+ettoreiovine@users.noreply.github.com>
2021-02-25 02:25:14 +0100

    Update and rename Iovine_riepilogoPython_2.py to riepilogoPython/Iovine_riepilogoPython_2.py

--- riepilogoPython/Iovine_riepilogoPython_2.py
+++ riepilogoPython/Iovine_riepilogoPython_2.py
@@ -1,3 +1,4 @@
+#Chiedi all'utente il nome ed il cognome e restituiscili in output separati da uno spazio.
 nome = str(input("Nome dell'utente: "))
 cognome = str(input("Cognome dell'utente: "))
 print(nome, cognome)
