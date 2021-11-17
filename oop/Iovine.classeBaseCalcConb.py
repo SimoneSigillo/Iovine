74df42e36c311395379d404900d605239137dedc
ettoreiovine <75394838+ettoreiovine@users.noreply.github.com>
2021-11-18 00:52:47 +0100

    Update Iovine.classeBaseCalcConb.py

--- oop/Iovine.classeBaseCalcConb.py
+++ oop/Iovine.classeBaseCalcConb.py
@@ -7,6 +7,7 @@ class calcComb():
         self.__N = len(stringa)
         self.__stringa = stringa
         self.__listStringa = list(stringa)
+        self.__anagrammi = anagrammi(self.__stringa)
 
     def get_stringa(self):
         return self.__stringa
@@ -20,6 +21,7 @@ class calcComb():
         istanza presenti
         '''
         self.__stringa = stringa
+
         return self.__stringa
 
     def charRipetuti(self):
@@ -28,8 +30,23 @@ class calcComb():
         il singolo carattere, il valore deve essere il numero di ripetizioni di quel carattere
         esempi di dictionary sono presenti nel file elementi_base/dictionary.py
         '''
+        carattere = {}
+        nCaratteri = 0
+        count = 0
+        for i in self.__listStringa:
+            if (i in carattere):  # se trova il carattere nel dictionary incrementa il suo valore
+                carattere[str(i)] += 1
+            else:
+                carattere[str(i)] = 1 # se non lo trova lo aggiunge
+        for i in carattere:
+            if carattere[i]>1: # se trova una lettera ripetuta
+                count += 1 # incrementa il numero di caratteri che si ripetono
+                nCaratteri += carattere[i] # incrementa il numero totale di ripetizioni
 
-    def confUtil(self):
+        return carattere
+    
+
+    def cerca(self):
         '''
         verificare se la STRINGA attributo di istanza è presente
         nel file word.italian.txt 
@@ -43,6 +60,7 @@ class calcComb():
         f.close()
 
         return stringaPresente
+
 
     def fattoriale(n):
         '''
@@ -65,7 +83,6 @@ class calcComb():
         ''' 
         implementare la formula del coefficiente binomiale a partire dal fattoriale
         '''
-        
         if k == n:
             return 1
         elif k == 1:         
@@ -73,10 +90,62 @@ class calcComb():
         elif k > n:          
             return 0
         else:
-            return calcComb.fattoriale(n) // (calcComb.fattoriale(n) * calcComb.fattoriale(n-k))
+            return calcComb.fattoriale(n) // (calcComb.fattoriale(k) * calcComb.fattoriale(n-k))
+            
+    def anagrammi(self):
+        '''
+        a partire dalla stringa (caratterizzante l'oggetto) si restituisca la lista di tutti
+        gli anagrammi possibili. E' presente uno script nel repo che esegue questo compito.
+        '''
+        from itertools import permutations
+
+        '''
+        Il seguente script genera una lista di anagrammi a partire da una stringa di caratteri data
+        '''
+        #generiamo tutte le possibili permutazioni e le inseriamo in una lista
+        permutazioni = list(permutations(self.__listStringa))
+
+        #inizializiamo una variabile stringa di appoggio e una lista dove salvarle
+        temp = ''
+        anagrammi = []
+
+        '''
+         il metodo permutations genera una lista di tuple, ognu tupla è una permutazione.
+         se scorriamo la lista attraverso un ciclo, possiamo scorrere gli elementi della tupla
+         per ricostruire la stringa
+        '''
+        for i in permutazioni:
+            for carattere in i:
+                # in temp concateno tutti gli elementi della tupla così da
+                # ottenere i singoli anagrammi della stringa iniziale
+                temp += carattere 
+
+            # aggiungo la parola ricostruita dalla tupla alla lista finale degli anagrammi
+            anagrammi.append(temp)
+            # "svuoto" la variabile temp così da ricostruire un secondo anagramma
+            temp = ''
+
+        return anagrammi
+
+    def confUtil(self):
+        '''
+        confUtil mette insieme diversi metodi basilari, lo scopo è:
+        a partire dalla lista di tutti gli anagrammi, verifica parola per parola la sua
+        presenza all'interno del file delle parole di senso compiuto, cancellando le altre.
+        si consiglia l'utilizzo di anagrammi e cerca, presenti nel repo e da trasformare in metodi.
+        è possibile in una seconda versione la ricefrca di parole in altre lingue. 
+        '''
+
+        anagrammiPuliti = []
+        for i in self.__anagrammi:
+            calcComb.cerca(i)
+            if stringaPresente == True:
+                anagrammiPuliti.append(i)
+
+        return anagrammiPuliti
+                
             
 
-        
         
 
 
@@ -100,7 +169,10 @@ class calcComb():
         '''
         generare e restituire la lista di permutazioni SENZA ripetizione
         '''
-        return 0
+        from itertools import permutations
+
+        permutazioni = list(permutations(self.__listStringa))
+        return permutazioni
 
     def permutConRip(self):
         '''
@@ -133,7 +205,6 @@ class calcComb():
         '''
         return 0
 
-
     def dispSemplConRip(self):
         '''
         generare e restituire la lista delle disposizioni semplici CON ripetizione
@@ -154,7 +225,7 @@ class calcComb():
         '''
         restituire il numero delle combinazioni CON ripetizione
         '''
-        CombSemplConRip = calcComb.fattoriale(self.__N+k-1) / (calcComb.fattoriale(k) * calcComb.fattoriale(self.__N-1))
+        CombSemplConRip = calcComb.coeffBinom(self.__N+k-1, k)
 
         return CombSemplConRip
 
@@ -171,4 +242,8 @@ class calcComb():
         '''
         return 0
 
+    # PROBABILITA'
+
+    def probConfUtil(self):
+        pass
 
