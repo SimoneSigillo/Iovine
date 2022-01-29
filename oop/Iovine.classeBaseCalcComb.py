d29d7fb36ef8318832e0b43276ab0d78fbb297ff
ettoreiovine <75394838+ettoreiovine@users.noreply.github.com>
2022-01-29 14:07:46 +0100

    Update Iovine.classeBaseCalcComb.py

--- oop/Iovine.classeBaseCalcComb.py
+++ oop/Iovine.classeBaseCalcComb.py
@@ -4,58 +4,49 @@ from itertools import permutations, combinations, combinations_with_replacement
 
 class calcComb():
 
-    def __init__(self, stringa):
-
-        self.__N = len(stringa)
-        self.__stringa = stringa
+    def __init__(self, stringa):                                                                    # Il metodo costruttore inizializza le variabili dell'istanza, ossia rispettivamente
+        self.__N = len(stringa)                                                                     # la lunghezza della stringa, la stringa, la lista delle lettere della stringa e la 
+        self.__stringa = stringa                                                                    # lista degli anagrammi della stringa                            
         self.__listStringa = list(stringa)
-        self.__anagrammi = calcComb.anagrammi2(stringa)
+        self.__anagrammi = self.anagrammi() 
 
-    def get_stringa(self):
+    def getStringa(self):                                                                           # Fornisce la stringa inserita come istanza.
         return self.__stringa
 
-    def get_listStringa(self):
+    def getListStringa(self):                                                                       # Fornisce la lista delle lettere della stringa inserita come istanza.
         return self.__listStringa
-
-    def setStringa(self, stringa):
+    
+    def setStringa(self, stringa):                                                                  # Permette di cambiare l'istanza con una nuova stringa.
         self.__stringa = stringa
         self.__N = len(stringa)
         self.__listStringa = list(stringa)
-        self.__anagrammi = calcComb.anagrammi2(stringa)
+        self.__anagrammi = self.anagrammi()
 
-    def charRipetuti(self):
-        carattere = {}
-        nCaratteri = 0
-        nRipetizioni = 0
+    def charRipetuti(self):                                                                         # Fornisce il numero di caratteri che si ripetono nella stringa, il numero di                                 
+        carattere = {}                                                                              # ripetizioni totale e un dictionary con le lettere come chiavi e il numero delle                                                                              #volte che il carattere si presenta nella parola come valore.                                                                            
         for i in self.__listStringa:
             if (i in carattere):  # se trova il carattere nel dictionary incrementa il suo valore
                 carattere[str(i)] += 1
             else:
                 carattere[str(i)] = 1 # se non lo trova lo aggiunge
-        for i in carattere:
-            if carattere[i]>1: # se trova una lettera ripetuta
-                nCaratteri += 1 # incrementa il numero di caratteri che si ripetono
-                nRipetizioni += carattere[i] # incrementa il numero totale di ripetizioni
+        ripetizioni = list(carattere.values())       
+        variabiliRipetizioni = [carattere, ripetizioni]
 
-        variabiliAnagrammi = [nCaratteri, nRipetizioni, carattere]
+        return variabiliRipetizioni
 
-        return variabiliAnagrammi
     
-
-    def cerca(self):
-        f = open("word.italian.txt", 'r')
+    def cerca(self):                                                                                # Controlla se la stringa inserita come istanza è presente nel vocabolario e quindi 
+        f = open("word.italian.txt", 'r')                                                           # se ha senso compiuto.
         riga = f.readline()
         stringaPresente = False
         for riga in f:
             if self.__stringa == riga[:-1]:
                 stringaPresente = True
-        
+
         return stringaPresente
 
-
-    
-            
-    def anagrammi(self):
+        
+    def anagrammi(self):                                                                            # Fornisce la lista di anagrammi della stringa inserita come istanza.
         permutazioni = list(permutations(self.__listStringa))
         temp = ''
         anagrammi = []
@@ -66,19 +57,19 @@ class calcComb():
             temp = ''
 
         return anagrammi
-    
 
-    def confUtil(self):
-        anagrammiPuliti = []
-        for i in self.__anagrammi:
+    
+    def confUtil(self):                                                                             # Fornisce la lista di anagrammi della stringa che hanno senso compiuto, servendosi   
+        anagrammiValidi = []                                                                        # della lista di anagrammi e controllando che ognuno sia presente nel vocabolario  
+        for i in self.__anagrammi:                                                                  # attraverso la funzione calcComb.cerca2().
             calcComb.cerca2(i)
             if  calcComb.cerca2(i) == True:
-                anagrammiPuliti.append(i)
+                anagrammiValidi.append(i)
 
-        return anagrammiPuliti
-                
-            
-    def cerca2(stringa):
+        return anagrammiValidi
+
+                     
+    def cerca2(stringa):                                                                            # Serve per il funzionamento del modulo self.confUtil().
         f = open("word.italian.txt", 'r')
         riga = f.readline()
         stringaPresente = False
@@ -86,33 +77,10 @@ class calcComb():
             if stringa == riga[:-1]:
                 stringaPresente = True
         
-        return stringaPresente #serve per il funzionamento del modulo confUtil
+        return stringaPresente 
 
 
-    def anagrammi2(stringa):
-        permutazioni = list(permutations(list(stringa)))
-        temp = ''
-        anagrammi = []
-        for i in permutazioni:
-            for carattere in i:
-                temp += carattere
-            anagrammi.append(temp)
-            temp = ''
-
-        return anagrammi #serve per il funzionamento del modulo confUtil e per self.__anagrammi
-
-
-    '''def confUtil2(stringa):
-        anagrammiPuliti = []
-        for i in calcComb.anagrammi2(stringa):
-            calcComb.cerca2(i)
-            if calcComb.cerca2(i) == True:
-                anagrammiPuliti.append(i)
-
-        return anagrammiPuliti''' #inutile
-
-
-    def fattoriale(n):
+    def fattoriale(n):                                                                              # Fornisce il fattoriale del numero inserito in input.
         if n < 0: 
             print("Il fattoriale di un numero negativo non esiste")
 
@@ -126,7 +94,8 @@ class calcComb():
                 n -= 1
             return fattoriale
 
-    def coeffBinom(n, k):
+
+    def coeffBinom(n, k):                                                                           # Fornisce il coefficiente binomiale di due numeri inseriti in input.
         if k == n:
             return 1
         elif k == 1:         
@@ -138,72 +107,80 @@ class calcComb():
         
 
 
-    # PERMUTAZIONI
-
-    def nPermutSenzaRip(self):
-        PermutSenzaRip = calcComb.fattoriale(self.__N)
-
-        return PermutSenzaRip
-
-    def nPermutConRip(self):
-        PermutConRip = calcComb.fattoriale(self.__N) / calcComb.charRipetuti(self)
-
-        return PermutConRip
-
-    def permutSenzaRip(self):
-        
-        return calcComb.anagrammi(self)
-
-    def permutConRip(self):
-        '''
-        generare e restituire la lista di permutazioni CON ripetizione
-        '''
-        return 0
-
     # DISPOSIZIONI
 
-    def nDispSemplSenzaRip(self, k):
+    def nDispSempl(self, k):                                                                        # Fornisce il numero di disposizioni semplici.
         if self.__N >= k:
-            DispSemplSenzaRip = calcComb.fattoriale(self.__N) / calcComb.fattoriale(self.__N-k)
+            nDispSempl = calcComb.fattoriale(self.__N) / calcComb.fattoriale(self.__N - k)
 
-        return DispSemplSenzaRip
+        return nDispSempl
 
-    def nDispSemplConRip(self, k):
-        DispSemplConRip = self.__N**k
 
-        return DispSemplConRip
+    def nDispConRip(self, k):                                                                       # Fornisce il numero di disposizioni con ripetizione.
+        nDispConRip = self.__N**k
 
-    def dispSemplSenzaRip(self, k):
-        listaDisposizioni = list(permutations(self.__stringa, k))
+        return nDispConRip
+
+
+    def dispSempl(self, k):                                                                         # Fornisce la lista di disposizioni semplici.
+        listaDispSempl = list(permutations(self.__listStringa, k))
         temp = ''
-        disposizioni = []
-        for i in listaDisposizioni:
+        dispSempl = []
+        for i in listaDispSempl:
             for carattere in i:
                 temp += carattere
             disposizioni.append(temp)
             temp = ''
         
-        return disposizioni
+        return dispSempl
 
-    def dispSemplConRip(self):
-        '''
-        generare e restituire la lista delle disposizioni semplici CON ripetizione
-        '''
-        return 0
+
+    def dispConRip(self):
+        pass
+
+
+
+    # PERMUTAZIONI
+
+    def nPermutSempl(self):                                                                         # Fornisce il numero di permutazioni semplici.
+        nPermutSempl = calcComb.fattoriale(self.__N)
+
+        return nPermutSempl
+
+
+    def nPermutConRip(self):                                                                        # Fornisce il numero di permutazioni con ripetizione.
+        denominatore = 1
+        for i in range(0, len(self.charRipetuti()[1])):
+            denominatore *= calcComb.fattoriale(self.charRipetuti()[1][i])
+        nPermutConRip = calcComb.fattoriale(self.__N) / denominatore
+
+        return nPermutConRip
+
+
+    def permutSenzaRip(self):                                                                       # Fornisce la lista di permutazioni senza ripetizioni, che coincide con la lista 
+        return self.anagrammi()                                                                     # di anagrammi
+
+
+    def permutConRip(self):
+        pass 
+
+
 
     # COMBINAZIONI
 
-    def nCombSemplSenzaRip(self, k):
-        CombSemplSenzaRip = calcComb.fattoriale(self.__N) / (calcComb.fattoriale(k) * calcComb.fattoriale(self.__N-k))
+    def nCombSempl(self, k):                                                                        # Fornisce il numero di combinazioni semplici.
+        nCombSempl = calcComb.fattoriale(self.__N) / (calcComb.fattoriale(k) * calcComb.fattoriale(self.__N-k))
 
-        return CombSemplSenzaRip
+        return nCombSempl
 
-    def nCombSemplConRip(self, k):
-        CombSemplConRip = calcComb.coeffBinom(self.__N+k-1, k)
 
-        return CombSemplConRip
+    def nCombConRip(self, k):                                                                       # Fornisce il numero di combinazioni con ripetizione.
+        nCombConRip = calcComb.coeffBinom(self.__N+k-1, k)
 
-    def combSenzaRip(self, k):
+        return nCombConRip
+
+
+    def combSempl(self, k):                                                                         # Fornisce la lista di combinazioni semplici.
         listaCombinazioni = list(combinations(self.__stringa, k))
         temp = ''
         combinazioni = []
@@ -216,7 +193,7 @@ class calcComb():
         return combinazioni
 
 
-    def combConRip(self, k):
+    def combConRip(self, k):                                                                        # Fornisce la lista di combinazioni con ripetizione.
         listaCombinazioni = list(combinations_with_replacement(self.__stringa, k))
         temp = ''
         combinazioni = []
@@ -228,17 +205,18 @@ class calcComb():
         
         return combinazioni
 
+
+
     # PROBABILITA'
 
-    def probConfUtil(self):
-        casiFavorevoli = 0
+    def probConfUtil(self):                                                                         # Fornisce la probabilità che un'anagramma della parola inserita come istanza abbia 
+        casiFavorevoli = 0                                                                          # senso compiuto.
         for i in self.__anagrammi: 
             Vtemp = calcComb.cerca2(i)
             if Vtemp == False:
                 None
             elif Vtemp == True:
                 casiFavorevoli += 1
-        
         probabilità = casiFavorevoli/(len(self.__anagrammi))
 
         return probabilità
